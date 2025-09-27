e3f0a47da940ade897e890e3cb625e30175b4084
Rhett Smithgall <rhettsmithgall@gmail.com>
2025-09-27 18:54:05 -0400

    new changes

--- Project1/main.c
+++ Project1/main.c
@@ -21,10 +21,10 @@ int main( int argc, char* argv[]){
     int lineCount = 0;    
     int address = 0x0; 
     char* token;
+    char* symName;
     
     //line counter
 	while( fgets(line, 1023, fp) != NULL   ){       //read the file line by line
-
         if ( line[0] == '#'   ) {                   //comment line
             lineCount++;
             continue;	
@@ -38,15 +38,21 @@ int main( int argc, char* argv[]){
 
         token = strtok(line, " \n\t\r");      //tokenize the line
 
-        if(isDirective(token) || isOpcode(token)) {     
-            token = strtok(line, " \n\t\r");  
-            if(isDirective(token) || isOpcode(token)) { //this indicates a symbol is named after a directive or opcode (not allowed)
-                printf("Error: Symbol name %s at line %d is a SIC assembly reserved instruction name\n", token, lineCount);
-                return -1;
+        
+        if(isDirective(token) || isOpcode(token)) {    
+            token = strtok(NULL, " \n\t\r");  
+            
+            if(token != NULL){
+                if(isDirective(token) || isOpcode(token)) { 
+                    printf("Error: Symbol name %s at line %d is a SIC assembly reserved instruction name\n", token, lineCount);
+                    return -1;
+                }
             }
             lineCount++;
+            address += 3;
             continue;
-        }          
+        }     
+ 
                            
         if(symbolExists(SYMTAB, token)) {     
             printf("Error: Symbol %s at line %d was already defined\n", token, lineCount);
@@ -58,19 +64,21 @@ int main( int argc, char* argv[]){
             return -1;
         }
 
-        struct symbol* currSym = insertSymbol(&SYMTAB, token, address, lineCount);
+        symName = token;
 
         token = strtok(NULL, " \n\t\r");   
         if(isDirective(token)) { 
                 if(strcmp(token, "START") == 0) {      
                     token = strtok(NULL, " \n\t\r");   
                     address = strtol(token, NULL, 16);
-                    currSym->address = address;
+                    lineCount++;   
                     continue;
                 }
 
-            if(strcmp(token, "END") == 0) {        
-                    break; 
+            if(strcmp(token, "END") == 0) { 
+                    address += 3;   
+                    lineCount++;   
+                    continue;    
                 }
 
             if(strcmp(token, "BYTE") == 0) {       
@@ -83,6 +91,8 @@ int main( int argc, char* argv[]){
                         return -1;
                     }
                     address += strlen(token);
+                    lineCount++;   
+                    continue;
                 }
                 else if(token[0] == 'X'){    
                     sscanf(token, "X'%[^']'", token); //remove X'__'
@@ -98,8 +108,9 @@ int main( int argc, char* argv[]){
                         return -1;
                     }
                     address += ceil(strlen(token)/2.0);
+                    lineCount++;   
+                    continue;
                 }
-                continue;
             }
 
             if(strcmp(token, "WORD") == 0) {  
@@ -110,34 +121,40 @@ int main( int argc, char* argv[]){
                     return -1;
                 }     
                 address += 3; 
+                lineCount++;   
                 continue;
             }
 
             if(strcmp(token, "RESB") == 0) {  
                 token = strtok(NULL, " \n\t\r");  
                 address += strtol(token, NULL, 10); 
+                lineCount++;   
                 continue;
             }
 
             if(strcmp(token, "RESW") == 0) {       
                 token = strtok(NULL, " \n\t\r");   
                 address += 3 * strtol(token, NULL, 10); 
+                lineCount++;   
                 continue;
             }
 
             if(strcmp(token, "RESR") == 0) {       
                 address += 3; 
+                lineCount++;   
                 continue;
             }
 
             if(strcmp(token, "EXPORTS") == 0) {    
                 address += 3; 
+                lineCount++;   
                 continue;
             }   
         }
 
-        lineCount++; 
-        address += 3;   
+        struct symbol* currSym = insertSymbol(&SYMTAB, symName, address, lineCount);
+        lineCount++;   
+        address += 3;
     }
     if(address > 0x8000){
         printf("ERROR: Program too large. Max size is 32767 bytes.\n");
