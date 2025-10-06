3c620184495a7b325f73dc7206204cb8281c29d4
Rhett Smithgall <rhettsmithgall@gmail.com>
2025-10-05 22:04:31 -0400

    fixed everything <3

--- Project1/isValidHex.c
+++ Project1/isValidHex.c
@@ -4,7 +4,7 @@ int isValidHex(char *input) {
     for (int i = 0; i < strlen(input); i++) {
         char c = input[i];
         if (!isxdigit(c)) {  
-            return i + 1;
+            return i + 1; //plus 1 because array index at 0 but we're sending the column number
         }
     }
     return 0;
